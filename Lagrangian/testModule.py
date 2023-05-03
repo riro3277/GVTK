9ee977b84218471b02307caead6df7dbc5724549
riro3277 <riro3277@colorado.edu>
2023-05-02 22:15:34 -0600

    Test failure

--- Lagrangian/testModule.py
+++ Lagrangian/testModule.py
@@ -16,7 +16,7 @@ class Testing():
     def Compare(self):
         #Reading standard files and test files from both locations for comparison
         StandardResults = self.StandardRoot + '/Lagrangian/StandardFiles/StandardFile0.txt'
-        TestResults = self.RootDir + 'TestFiles/TestFile0.txt'
+        TestResults = self.RootDir + 'TestFiles/TestFile1.txt'
 
         StandardFile = open(StandardResults,'r')
         TestFile = open(TestResults,'r')
